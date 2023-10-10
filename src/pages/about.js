2ddfbe0c3367c06dcf4a3db687c5e878eab777cb
Tongyu Zhou <tongyuzhou98@gmail.com>
2023-10-10 18:15:22 -0400

    changed footer

--- src/pages/about.js
+++ src/pages/about.js
@@ -54,7 +54,7 @@ export class About extends Component {
                         </div>
                     </div>
                     <div className="flex-row flex-center footer">
-                        <footer className="bottom">Made with 🍄 by Tongyu Zhou</footer>
+                        <footer className="bottom">made while listening to lots of city pop 🌃🎶 by Tongyu Zhou</footer>
                     </div>
                 </div>
         )
